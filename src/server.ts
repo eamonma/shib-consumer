06cf02460b1f95b6de8b841923eb1562ad899f81
Eamon Ma <m@eamonma.com>
2023-02-08 17:51:39 -0500

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,75 @@
+/**
+ * Setup express server.
+ */
+
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import path from 'path';
+import helmet from 'helmet';
+import express, { Request, Response, NextFunction } from 'express';
+import logger from 'jet-logger';
+
+import 'express-async-errors';
+
+import EnvVars from '@src/constants/EnvVars';
+import HttpStatusCodes from '@src/constants/HttpStatusCodes';
+
+import { NodeEnvs } from '@src/constants/misc';
+import { RouteError } from '@src/other/classes';
+
+
+// **** Variables **** //
+
+const app = express();
+
+
+// **** Setup **** //
+
+// Basic middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser(EnvVars.CookieProps.Secret));
+
+// Show routes called in console during development
+if (EnvVars.NodeEnv === NodeEnvs.Dev) {
+  app.use(morgan('dev'));
+}
+
+// Security
+if (EnvVars.NodeEnv === NodeEnvs.Production) {
+  app.use(helmet());
+}
+
+// Add APIs, must be after middleware
+
+// Add error handler
+app.use((
+  err: Error,
+  _: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction,
+) => {
+  if (EnvVars.NodeEnv !== NodeEnvs.Test) {
+    logger.err(err, true);
+  }
+  let status = HttpStatusCodes.BAD_REQUEST;
+  if (err instanceof RouteError) {
+    status = err.status;
+  }
+  return res.status(status).json({ error: err.message });
+});
+
+
+// Nav to login pg by default
+app.get('/', (req: Request, res: Response) => {
+  const { mail, utorid, affiliation, "persistent-id": pId } = req.headers;
+  console.log(mail, utorid, affiliation, pId);
+  
+  res.sendStatus(200)
+  // res.sendFile('login.html', { root: viewsDir });
+});
+
+// **** Export default **** //
+
+export default app;
